676c3558de6b52f4e090caec988b4f933fd031e8
clericbob <ccscoggins121@gmail.com>
2021-07-09 00:56:42 -0400

    Added string conversion methods (via hex) to GBTile

--- groutboy/GBTile.java
+++ groutboy/GBTile.java
@@ -6,19 +6,47 @@ package groutboy;
  *	purpose: Rewrite of original.  Provides implementation for single 8x8 pixel tile
  */
 
+import java.util.StringReader;
+
+import java.io.IOException;
 
 public class GBTile{
 	private int [] pixel;
 	
+	/**	default constructor
+	 *	purpose: creates a new, blank tile
+	 */
 	public GBTile(){
 		pixel = new int[64];
 	}
+	
+	/**	constructor (String)
+	 *	purpose: loads hexadecimal 2BPP data into the tile
+	 */
+	public GBTile(String hexString){
+		StringReader hexInput = new StringReader(hexString);
+		pixel = new int[64];
+		short currByte;
+		for(int i=0; i<16; i++){
+			try{
+				currByte = hexInput.read();
+			} catch (IOException E){
+				System.err.println("Exception occurred on reading tile from hex data.");
+				E.printStackTrace();
+			}
+			for(int j=0; j<4; j++){
+				pixel[i*4+j] = currByte & 0x03;
+				currByte >> 2;
+			}
+		}
+	}
+	
 	
 	public boolean setPixel(int x, int y, int newValue){
 		if(x < 0 || y < 0 ||
 			x > 8 || y > 8 ||
 			newValue < 0 || newValue > 3){
-			System.err.println("Error: attempted to set invalid pixel with values: x=" + x + ", y=" + y + ", val=" + newValue);
+			System.err.println("Error: attempted to set pixel with invalid values: x=" + x + ", y=" + y + ", val=" + newValue);
 			return false;
 		}
 		pixel[y * 8 + x] = newValue;
@@ -38,6 +66,24 @@ public class GBTile{
 		for(int i=0; i<64; i++){
 			pixel[i] = palIndex;
 		}
+	}
+	
+	/**	method: getHexRepresentation
+	 *	purpose: creates a 2bpp hex representation of the tile
+	 *		for saving purposes
+	 */
+	public String getHexRepresentation(){
+		short currByte = 0;
+		StringBuffer buf = new StringBuffer();
+		for(int i=0; i<16; i++){
+			for(int j=0; j<4; j++){
+				currByte << 2;
+				currByte |= pixel[i*4 + j];
+			}
+			buf.append(String.toHexString(currByte & 0xFF)); //and is failsafe in case numbers got weird.
+			currByte = 0;
+		}
+		return buf.toString();
 	}
 	
 }
